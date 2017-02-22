0a476273481162fa1369473dd6ff68530f7be11b
DESKTOP-Q8TLR3L\Keaghan's PC <keaghan.b.potts@gmail.com>
2017-02-23 07:51:36 +1000

    +int

---
+++ Assets/victim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class victim : MonoBehaviour {
+    public Rigidbody RB;
+    public float Force = 1000f;
+
+	// Use this for initialization
+	void Start () {
+        RB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            RB.AddForce(transform.forward * Force);
+            //Decellerate when key is not down.
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            RB.AddForce(-transform.right * Force);
+            //Decellerate when key is not down.
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            RB.AddForce(transform.right * Force);
+            //Decellerate when key is not down.
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            RB.AddForce(-transform.forward * Force);
+            //Decellerate when key is not down.
+        }
+
+    }
+}
